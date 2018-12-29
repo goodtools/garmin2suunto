bfca3b8a74b59f1a88ad68c83833b1934a9f039a
wuzhong <wuzhong@taobao.com>
2018-12-29 18:35:04 +0800

    🐛 mem leak

--- core/src/main/java/cn/lujiawu/garmin2suunto/garmin/GarminConnectApi.java
+++ core/src/main/java/cn/lujiawu/garmin2suunto/garmin/GarminConnectApi.java
@@ -1,6 +1,7 @@
 package cn.lujiawu.garmin2suunto.garmin;
 
 import cn.lujiawu.garmin2suunto.util.OkHttpClientManager;
+import okhttp3.Call;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
@@ -11,31 +12,34 @@ public class GarminConnectApi {
 
         OkHttpClient client = OkHttpClientManager.getInstance();
 
-        Response response = client.newCall(GarminConfiguration.getLoginPageRequest()).execute();
-
-        if (!response.isSuccessful()) {
-            return false;
+        try(Response response = client.newCall(GarminConfiguration.getLoginPageRequest()).execute();){
+            if (!response.isSuccessful()) {
+                return false;
+            }
         }
 
-        Response submit = client.newCall(GarminConfiguration.getLoginSubmitPostRequest(userName, password)).execute();
-        if (!submit.isSuccessful()) {
-            return false;
+        try(Response submit = client.newCall(GarminConfiguration.getLoginSubmitPostRequest(userName, password)).execute();){
+            if (!submit.isSuccessful()) {
+                return false;
+            }
         }
 
         String afterLoginUrl = GarminConfiguration.MODERN_URL;
         int times = 0;
         while (true) {
             Request afterRequest = new Request.Builder().get().url(afterLoginUrl).build();
-            Response execute = client.newCall(afterRequest).execute();
-            if (!execute.isRedirect()) {
-                break;
-            }
-            afterLoginUrl = execute.header("Location");
-            if (afterLoginUrl.startsWith("/")) {
-                afterLoginUrl = GarminConfiguration.GARMIN_CN + afterLoginUrl;
-            }
-            if (times++ > 5) {
-                return false;
+            Call call = client.newCall(afterRequest);
+            try(Response redirectResponse = call.execute();){
+                if (!redirectResponse.isRedirect()) {
+                    break;
+                }
+                afterLoginUrl = redirectResponse.header("Location");
+                if (afterLoginUrl.startsWith("/")) {
+                    afterLoginUrl = GarminConfiguration.GARMIN_CN + afterLoginUrl;
+                }
+                if (times++ > 5) {
+                    return false;
+                }
             }
         }
 
